dcde756261a84b7b3cc6b050d963dc4d25c851bd
alebul <master8423@gmail.com>
2022-05-24 14:54:22 +0300

    add fields of registration

--- app/src/main/java/com/example/freecodecamp_challenge/MainActivity.java
+++ app/src/main/java/com/example/freecodecamp_challenge/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.freecodecamp_challenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtName;
+    private EditText edtEmail;
+    private EditText edtPwd;
+    private EditText edtRetrPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtName = findViewById(R.id.txtName);
+        edtName = findViewById(R.id.txtEmail);
+        edtName = findViewById(R.id.txtPassword);
+        edtName = findViewById(R.id.txtRePassword);
+
+
     }
 }
